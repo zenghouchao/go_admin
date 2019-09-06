3468e0ec2c592e8a5e67ac6dfca363b97fa8ddb7
unknown <greatjerry@foxmail.com>
2019-09-06 15:49:28 +0800

    update

---
+++ router/router.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+	"go_admin/handler"
+	"net/http"
+)
+
+func init() {
+	// 静态文件路由注册
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.HandleFunc("/", handler.IndexHandler)
+	http.HandleFunc("/login", handler.DoLogin)
+	http.HandleFunc("/home", handler.LoginInterceptor(handler.AdminHandler))
+	http.HandleFunc("/home/welcome", handler.LoginInterceptor(handler.WelcomeHandler))
+	http.HandleFunc("/logout", handler.LoginInterceptor(handler.LogoutHandler))
+	// 文章管理
+	http.HandleFunc("/article", handler.LoginInterceptor(handler.ArticleListHandler))
+	http.HandleFunc("/article/add", handler.LoginInterceptor(handler.ArticleAddHandler))
+	http.HandleFunc("/article/delete", handler.LoginInterceptor(handler.ArticleDeleteHandler))
+	http.HandleFunc("/article/update", handler.LoginInterceptor(handler.ArticleUpdateHandler))
+	http.HandleFunc("/article/edit/", handler.LoginInterceptor(handler.ArticleEditPageHandler))
+
+	// 栏目分类
+	http.HandleFunc("/cate", handler.LoginInterceptor(handler.CateListHandler))
+	http.HandleFunc("/cate/add", handler.LoginInterceptor(handler.CateAddHandler))
+	http.HandleFunc("/cate/delete", handler.LoginInterceptor(handler.CateDelHandler))
+	http.HandleFunc("/cate/saveState", handler.LoginInterceptor(handler.CateStatusSaveHandler))
+}
